f1716612a0d1fdad9abd7e594445ad1c34835d8d
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2015-11-24 22:38:22 -0800

    Able to tweet

--- tweet.py
+++ tweet.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Install pyserial first
+from datetime import timedelta
+from datetime import datetime
+
+# Install pyserial
 import serial
 
+# Install python-twitter
+import twitter
+
 ser = serial.serial_for_url("/dev/cu.usbmodem1421", 9600, timeout=1)
+
+api = twitter.Api(
+    consumer_key="consumer_key",
+    consumer_secret="consumer_secret",
+    access_token_key="access_token",
+    access_token_secret="access_token_secret")
+
+tweet_interval = timedelta(minutes=15)
+last_post = None
 
 while True:
     data = ser.readline().strip()
@@ -14,4 +29,10 @@ while True:
         except ValueError as e:
             print e
             continue
-        print h, t, hi
+
+        status = "Temperature: {}℃ \nHumidity: {}% \nHeat Index: {}℃".format(h, t, hi)
+        print status
+        print "\n"
+        if (not last_post) or (datetime.now() - last_post > tweet_interval):
+            api.PostUpdate(status)
+            last_post = datetime.now()
